420d997e09869ebf22df8dc927a0ef9668b479c6
hartogsmith <ronald.d.smith@gmail.com>
2024-02-11 16:35:58 -0600

    good enough? 3

--- web/themes/custom/b5_womanup/js/load.js
+++ web/themes/custom/b5_womanup/js/load.js
@@ -3,3 +3,26 @@ const theNavbar = document.querySelector("nav.navbar-main");
 theButton.addEventListener("click", (event) => {
     theNavbar.classList.toggle("open");
 });
+
+
+//use window.scrollY
+var scrollY = window.scrollY;
+
+function add_class_on_scroll() {
+    theNavbar.classList.add("fade-in");
+}
+
+function remove_class_on_scroll() {
+    theNavbar.classList.remove("fade-in");
+}
+
+window.addEventListener('scroll', function(){ 
+    scrollY = window.scrollY;
+    if(scrollY > 10){
+        add_class_on_scroll();
+    }
+    else {
+        remove_class_on_scroll();
+    }
+    console.log(scrollY);
+});
