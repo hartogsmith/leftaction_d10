6ff3331781f54604c3e790b759cb84c4325cdd63
hartogsmith <ronald.d.smith@gmail.com>
2024-02-11 16:02:25 -0600

    wu swag 8

--- web/themes/custom/b5_womanup/js/load.js
+++ web/themes/custom/b5_womanup/js/load.js
@@ -1,3 +1,6 @@
-jQuery( 'btn.navbar-toggler' ).on( "click", function() {
-    jQuery('nav.bg-primary').toggleClass('open');
-  } );
+// mobile nav toggle
+const nav_btn = document.querySelector('btn.navbar-toggler');
+const nav_bar = document.querySelector('nav.navbar-main');
+nav_btn.addEventListener("click", (event) => {
+    nav_bar.classList.toggle('open');;
+});
