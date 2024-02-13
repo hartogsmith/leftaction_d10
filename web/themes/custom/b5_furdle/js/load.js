9c6e9404ddd894ba335a90b3d2a64c826a7ce03a
hartogsmith <ronald.d.smith@gmail.com>
2024-02-13 10:24:05 -0600

    furdle game 100

--- web/themes/custom/b5_furdle/js/load.js
+++ web/themes/custom/b5_furdle/js/load.js
@@ -5,29 +5,6 @@ theButton.addEventListener("click", (event) => {
 });
 
 
-//use window.scrollY
-var scrollpos = window.scrollY;
-
-function add_class_on_scroll() {
-    theNavbar.classList.add("fade-in");
-}
-
-function remove_class_on_scroll() {
-    theNavbar.classList.remove("fade-in");
-}
-
-window.addEventListener('scroll', function(){ 
-    //Here you forgot to update the value
-    scrollpos = window.scrollY;
-
-    if(scrollpos > 10){
-        add_class_on_scroll();
-    }
-    else {
-        remove_class_on_scroll();
-    }
-    //console.log(scrollpos);
-});
 
 var wow = new WOW(
     {
